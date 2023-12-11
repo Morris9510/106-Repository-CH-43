626b44c65b6486416de09a4c3daa57b2db0c0b9f
Morris <morris9510@gmail.com>
2023-12-10 19:23:45 -0600

    106 Project

---
+++ scripts.js/task.js
@@ -0,0 +1,14 @@
+class Task {
+  constructor(title, description, color, date, status, budget) {
+    this.title = title;
+    this.description = description;
+    this.color = color;
+    this.date = date;
+    this.status = status;
+    this.budget = budget;
+
+    //also add this other one
+    this.name = "Morris";
+    //this is the key that will be used to get connected to the server
+  }
+}
